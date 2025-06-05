d9de0d6d60703ed843bb2219041abab50f79545b
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-06-04 23:51:53 -0300

    Add business page updated form connected to backend

--- src/pages/add_business.js
+++ src/pages/add_business.js
@@ -4,6 +4,11 @@ import {useState} from "react";
 const AddBusiness = () => {
     const [businessName, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [address, setAddress] = useState('');
+    const [phoneNum, setPhoneNum] = useState('');
+    const [email, setEmail] = useState('');
+    const [website, setWebsite] = useState('');
 
     const handleSubmit = async (e)=>{
         e.preventDefault();
@@ -12,7 +17,7 @@ const AddBusiness = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({businessName, description})
+            body: JSON.stringify({businessName, description, category, address, phoneNum, email, website})
         });
         const data = await response.json();
         console.log(data);
@@ -30,41 +35,48 @@ const AddBusiness = () => {
                        onChange={(e)=> setName(e.target.value)}
                        required={true}
                 />
-                {/*<label className={'mt-1'}>Category</label>*/}
-                {/*<select className={'border-gray-400 border rounded h-10 mt-1'} required={true}>*/}
-                {/*    <option>Select an Option</option>*/}
-                {/*    <option>Restaurant</option>*/}
-                {/*    <option>Transportation</option>*/}
-                {/*    <option>Accommodation</option>*/}
-                {/*    <option>Tours</option>*/}
-                {/*</select>*/}
+                <label className={'mt-1'}>Category</label>
+                <select onChange={(e)=>setCategory(e.target.value)}  className={'border-gray-400 border rounded h-10 mt-1'} required={true}>
+                    <option>Select an Option</option>
+                    <option value={'Restaurant'}>Restaurant</option>
+                    <option value={'Transportation'}>Transportation</option>
+                    <option value={'Accommodation'}>Accommodation</option>
+                    <option value={'Tours'}>Tours</option>
+                </select>
                 <label className={'mt-1'}>Description</label>
                 <textarea className={'border border-gray-400 h-24 rounded mt-1 resize-none focus:outline-0 pl-1'}
                           placeholder={'Describe your business in detail'} required={true}
                           value={description}
                           onChange={(e)=>setDescription(e.target.value)}
                 ></textarea>
-                {/*<label className={'mt-1'}>Address</label>*/}
-                {/*<input className={'h-10 border border-gray-400 rounded mt-1 focus:outline-0 pl-1'}*/}
-                {/*       placeholder={'Enter Address'}*/}
-                {/*       required={true}*/}
-                {/*/>*/}
-                {/*<label className={'mt-1'}>Phone Number</label>*/}
-                {/*<input className={'border border-gray-400 rounded h-10 mt-1 focus:outline-0 pl-1'}*/}
-                {/*       placeholder={'Enter Phone Number'}*/}
-                {/*       required={true}*/}
-                {/*/>*/}
-                {/*<label className={'mt-1'}>Email</label>*/}
-                {/*<input type={'email'} className={'border border-gray-400 h-10 rounded mt-1 focus:outline-0 pl-1'}*/}
-                {/*       placeholder={'Enter email'}*/}
-                {/*       required={true}*/}
-                {/*/>*/}
-                {/*<label className={'mt-1'}>Website</label>*/}
-                {/*<input className={'border border-gray-400 rounded h-10 mt-1 focus:outline-0 pl-1'}*/}
-                {/*       placeholder={'Enter URL'}*/}
-                {/*       value={''}*/}
-                {/*       required={true}*/}
-                {/*/>*/}
+                <label className={'mt-1'}>Address</label>
+                <input className={'h-10 border border-gray-400 rounded mt-1 focus:outline-0 pl-1'}
+                       placeholder={'Enter Address'}
+                       value={address}
+                       onChange={(e) => setAddress(e.target.value)}
+                       required={true}
+                />
+                <label className={'mt-1'}>Phone Number</label>
+                <input className={'border border-gray-400 rounded h-10 mt-1 focus:outline-0 pl-1'}
+                       placeholder={'Enter Phone Number'}
+                       value={phoneNum}
+                       onChange={(e)=>setPhoneNum(e.target.value)}
+                       required={true}
+                />
+                <label className={'mt-1'}>Email</label>
+                <input type={'email'} className={'border border-gray-400 h-10 rounded mt-1 focus:outline-0 pl-1'}
+                       placeholder={'Enter email'}
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}
+                       required={true}
+                />
+                <label className={'mt-1'}>Website</label>
+                <input className={'border border-gray-400 rounded h-10 mt-1 focus:outline-0 pl-1'}
+                       placeholder={'Enter URL'}
+                       value={website}
+                       onChange={(e) => setWebsite(e.target.value)}
+                       required={true}
+                />
                 {/*<label className={'mt-1'}>Upload Photos</label>*/}
                 {/*/!*<input type={'file'} className={'mt-1 border-dashed'} required={true}/>*!/*/}
                 {/*<div className={'dropzone mt-1 border border-gray-400 border-dashed h-36 w-full rounded-xl justify-center items-center'}>*/}
