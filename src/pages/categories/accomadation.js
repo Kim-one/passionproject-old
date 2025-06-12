d75d7f37eb25062245b5f2b5258d1a3e98213b2e
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-06-11 23:59:16 -0300

    different categories added

---
+++ src/pages/categories/accomadation.js
@@ -0,0 +1,11 @@
+import '../../stylesheets/styles.css';
+
+const Accommodation =()=>{
+    return (
+        <div>
+            <h1>Accommodation</h1>
+        </div>
+    )
+}
+
+export default Accommodation;
