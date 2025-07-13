ab62e6bb746f539aadaaa848a2eab1c28eac3e5b
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-07-13 10:57:09 -0300

    Displaying map on homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -126,17 +126,17 @@ const Home=()=>{
                         </div>
                     </div>
                     <div>
-                        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                            <TileLayer
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <Marker position={[51.505, -0.09]}>
-                                <Popup>
-                                    A pretty CSS3 popup. <br /> Easily customizable.
-                                </Popup>
-                            </Marker>
-                        </MapContainer>
+                        {/*<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>*/}
+                        {/*    <TileLayer*/}
+                        {/*        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'*/}
+                        {/*        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
+                        {/*    />*/}
+                        {/*    <Marker position={[51.505, -0.09]}>*/}
+                        {/*        <Popup>*/}
+                        {/*            A pretty CSS3 popup. <br /> Easily customizable.*/}
+                        {/*        </Popup>*/}
+                        {/*    </Marker>*/}
+                        {/*</MapContainer>*/}
                     </div>
                     {/*<p>Kingston</p>*/}
                     {/*<p>St.Andrew</p>*/}
